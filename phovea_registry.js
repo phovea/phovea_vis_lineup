1d5af7b8e317f75912c59d519dff3b3582b1dc32
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-11-01 18:19:00 +0100

    restructure

---
+++ phovea_registry.js
@@ -0,0 +1,16 @@
+/* *****************************************************************************
+ * Caleydo - Visualization for Molecular Biology - http://caleydo.org
+ * Copyright (c) The Caleydo Team. All rights reserved.
+ * Licensed under the new BSD license, available at http://caleydo.org/license
+ **************************************************************************** */
+
+import {register} from 'phovea_core/src/plugin';
+
+/**
+ * build a registry by registering all phovea modules
+ */
+//other modules
+register(require('phovea_core/phovea.js'));
+register(require('phovea_d3/phovea.js'));
+//self
+register(require('./phovea.js'));
